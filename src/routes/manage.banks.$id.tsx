4d4f1c7c203ffe7d577003cca0574664a7b2e905
klassiqgrafikz <klassiqgrafikz@gmail.com>
2026-07-30 12:41:44 +0100

    Switch to new Supabase project and remove Dashboard Layout Designer

--- src/routes/manage.banks.$id.tsx
+++ src/routes/manage.banks.$id.tsx
@@ -465,16 +465,6 @@ function BankOverview() {
             <Button
               variant="outline"
               className="justify-start"
-              asChild
-            >
-              <Link to="/manage/banks/$id/dashboard-designer" params={{ id }}>
-                <LayoutDashboard className="mr-2 h-4 w-4" /> Dashboard Layout Designer
-                <span className="ml-1">⭐</span>
-              </Link>
-            </Button>
-            <Button
-              variant="outline"
-              className="justify-start"
               onClick={() => navigate({ to: "/admin/$section", params: { section: "customers" } })}
             >
               <Users className="mr-2 h-4 w-4" /> Manage Customers
